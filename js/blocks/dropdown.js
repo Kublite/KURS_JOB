aa283ec4faa24767c42403ebd2e07cb4c3f84151
Kublite <Stepanida525@gmail.com>
2024-12-11 21:18:56 +0300

    new desktop dropdown

---
+++ js/blocks/dropdown.js
@@ -0,0 +1,22 @@
+const buttonElement = document.querySelectorAll(".dropdown-menu__button");
+buttonElement.forEach((button) =>
+  button.addEventListener("click", function (event) {
+    document
+      .querySelectorAll(".header__menu-item")
+      .forEach((item) => item.classList.remove("dropdown-menu__open"));
+    event.isClick = true;
+    button.parentElement.classList.toggle("dropdown-menu__open");
+    let;
+  })
+);
+document.body.addEventListener("click", function (event) {
+  if (
+    event._isClick == true ||
+    event.target.classList.contains("dropdown-menu__button") == true ||
+    event.target.classList.contains("dropdown-menu__list") == true
+  )
+    return;
+  document
+    .querySelectorAll(".header__menu-item")
+    .forEach((item) => item.classList.remove("dropdown-menu__open"));
+});
