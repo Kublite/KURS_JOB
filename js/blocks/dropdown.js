9d11cab4ff4da8d179ae50feb22c16c3781c1bc4
Kublite <Stepanida525@gmail.com>
2024-12-11 22:41:55 +0300

    fix dropdown

--- js/blocks/dropdown.js
+++ js/blocks/dropdown.js
@@ -1,9 +1,11 @@
 const buttonElement = document.querySelectorAll(".dropdown-menu__button");
 buttonElement.forEach((button) =>
   button.addEventListener("click", function (event) {
-    document
-      .querySelectorAll(".header__menu-item")
-      .forEach((item) => item.classList.remove("dropdown-menu__open"));
+    document.querySelectorAll(".header__menu-item").forEach((item) => {
+      if (item.querySelector(".dropdown-menu__button") !== button) {
+        item.classList.remove("dropdown-menu__open");
+      }
+    });
     event.isClick = true;
     button.parentElement.classList.toggle("dropdown-menu__open");
     let;
