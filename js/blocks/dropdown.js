3d1fdbbf45ea0e55cd1c6b39c9e737f18173a497
Kublite <Stepanida525@gmail.com>
2024-12-15 12:30:15 +0300

    dropdown mobile

--- js/blocks/dropdown.js
+++ js/blocks/dropdown.js
@@ -4,21 +4,42 @@ buttonElement.forEach((button) =>
     document.querySelectorAll(".header__menu-item").forEach((item) => {
       if (item.querySelector(".dropdown-menu__button") !== button) {
         item.classList.remove("dropdown-menu__open");
+        if (
+          item.lastElementChild.classList.contains("dropdown-menu__list") ==
+          true
+        ) {
+          item.lastElementChild.style.position = "absolute";
+        }
       }
     });
     event.isClick = true;
     button.parentElement.classList.toggle("dropdown-menu__open");
-    let;
+    const dropdownList = button.nextElementSibling;
+    if (
+      button.classList.contains("mobile-dropdown__button") == true &&
+      button.parentElement.classList.contains("dropdown-menu__open")
+    ) {
+      dropdownList.style.position = "relative";
+    } else {
+      dropdownList.style.position = "absolute";
+    }
   })
 );
 document.body.addEventListener("click", function (event) {
   if (
     event._isClick == true ||
     event.target.classList.contains("dropdown-menu__button") == true ||
-    event.target.classList.contains("dropdown-menu__list") == true
-  )
+    event.target.classList.contains("mobile-overlay") == true ||
+    event.target.classList.contains("mobile-overlay__list") == true ||
+    event.target.classList.contains("button__burger-button") == true
+  ) {
     return;
+  }
   document
     .querySelectorAll(".header__menu-item")
     .forEach((item) => item.classList.remove("dropdown-menu__open"));
+
+  document.querySelectorAll(".dropdown-menu__list").forEach((list) => {
+    list.style.position = "absolute";
+  });
 });
