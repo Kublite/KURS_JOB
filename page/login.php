d769ac6ffda4b7b377113111b32f8149468f78c3
Kublite <Stepanida525@gmail.com>
2025-03-07 18:24:24 +0300

    login form

---
+++ page/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Вход</title>
+    <link rel="icon" href="../img\favicon.ico" type="image/x-icon" />
+    <link rel="stylesheet" href="../styles/styles.css" />
+  </head>
+  <body>
+    <?php 
+      include '../templates\blocks/login.html'; 
+    ?>
+  </body>
+</html>
